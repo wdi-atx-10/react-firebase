0f4ffa172d080caee055adcdc982ed37add49c95
patientplatypus <pweyand@gmail.com>
2017-04-20 20:37:14 -0500

    if you want it responsive ill give you my reaction

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './Meme.css';
 
 class Meme extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      meme: this.props.img
+    }
+  }
+
   render(){
     return(
         <div className="meme">
-
+          <img src={this.state.meme} alt=""/>
         </div>
     )
   };
--- src/components/Motivation.js
+++ src/components/Motivation.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
 import MotivationList from './MotivationList'
 import { database, firebaseListToArray } from '../utils/firebase';
+import './Motivation.css';
 
 class Motivation extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      motivations: []
+      motivations: [],
     }
   }
+
   componentWillMount(){
-    //Write
-    //Your
-    //Code
-    //Here!
+    database.ref('/motivational')
+      .on('value', data => {
+        const results = firebaseListToArray(data.val());
+        console.log('results', results);
+        this.setState({
+          motivations: results
+        });
+      });
   }
 
   render(){
-    //Write your const to pass down the state to the MotivationList.js Component
-    //
+    const motivationitems = this.state.motivations.map(item => {
+          return <MotivationList id={item.id} description={item.description} memes={item.memes}/>
+    });
+
 
     return(
       <section>
       <div className="row memes">
         {motivationitems}
       </div>
-    </section>
+      </section>
     )
   };
 }
