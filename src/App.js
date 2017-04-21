dfd34af9105ec79f5ad60a88e76b2be5ed4a7e28
Robert Saunders <rtsaunders19@gmail.com>
2017-04-20 21:10:40 -0500

    try

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App container">
-        <Header></Header>
-        <Motivation></Motivation>
+        <Header />
+        <Motivation />
       </div>
     );
   }
--- src/components/Motivation.js
+++ src/components/Motivation.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
 import MotivationList from './MotivationList'
 import { database, firebaseListToArray } from '../utils/firebase';
+import firebase from 'firebase';
 
 class Motivation extends Component {
   constructor(props) {
     super(props);
+
+    this.ref = firebase.database().ref();
     this.state = {
       motivations: []
     }
   }
+
   componentWillMount(){
-    //Write
-    //Your
-    //Code
-    //Here!
-  }
+    database.ref('/motivational')
+      .on('value', data => {
+          const results = firebaseListToArray(data.val());
+          console.log('motivation', results);
+
+          this.setState({
+            motivations: results
+          });
+        });
+}
 
   render(){
-    //Write your const to pass down the state to the MotivationList.js Component
-    //
+    const listofmotivations = this.state.motivations.map((item) => {
+      return <MotivationList id={ item.id } description={item.description} alt="" />
+    })
 
     return(
       <section>
       <div className="row memes">
-        {motivationitems}
+          {listofmotivations}
       </div>
-    </section>
+      </section>
     )
   };
 }
