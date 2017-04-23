a8c38f587616a5267e4ea7829db0fbb6913c6fbd
Tanya Selvog <tanyaselvog@gmail.com>
2017-04-23 12:43:11 -0500

    homework

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Motivation from './components/Motivation';
+import MotivationaList from './components/MotivationaList';
 
 class App extends Component {
   render() {
