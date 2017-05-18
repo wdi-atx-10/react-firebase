05625abcd40187dbab45ad47873f649a48e6620c
melissa pringle <melissa.a.acree@gmail.com>
2017-05-18 09:00:07 -0500

    submit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header';
-import Motivation from './components/Motivation';
+  import Header from './components/Header';
+  import Motivation from './components/Motivation';
+ +import MotivationList from './components/MotivationList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App container">
-        <Header></Header>
-        <Motivation></Motivation>
-      </div>
-    );
-  }
-}
+  class App extends Component {
+    render() {
+     return (
+       <div className="App container">
+         <Header></Header>
+         <Motivation></Motivation>
+       </div>
+     );
+   }
+ }
 
-export default App;
+ export default App;
